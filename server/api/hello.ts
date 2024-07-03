39db904b77c67ed4621bdfbf16d015b9cde5f132
Tazhetdinov Artyom <83722210+camnone@users.noreply.github.com>
2024-07-03 19:26:58 +0300

    update manifest

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -15,11 +15,16 @@ export default defineEventHandler(async (event) => {
     value["url"] = body["url"]
     value["descriptions"] = body["descriptions"]
     value["url_handlers"][0]["origin"] = body["url"];
-    value["icons"][0]["src"] = body["icons"]["512"]
-    value["icons"][1]["src"] = body["icons"]["384"]
-    value["icons"][2]["src"] = body["icons"]["256"]
-    value["icons"][3]["src"] = body["icons"]["192"]
 
+    console.log(body["icons"]);
+
+
+    value["icons"][0]["src"] = body["icons"]["192"]
+    value["icons"][1]["src"] = body["icons"]["256"]
+    value["icons"][2]["src"] = body["icons"]["384"]
+    value["icons"][3]["src"] = body["icons"]["512"]
+
+    console.log(value["icons"]);
     fs.writeFileSync(filePath, JSON.stringify(value, null, 2));
 
     return value
